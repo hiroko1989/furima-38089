73fc16ea6d558c7bdf9a7eb2842202a266a593ce
hiroko1989 <mwam0615@gmail.com>
2022-07-13 23:15:25 +0900

    ストロングパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,6 +9,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["FURIMA_USER"] && password == ENV["FURIMA_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_date ,:nickname,:family_name,:first_name,:ruby_family_name, :ruby_first_name])
   end
 
 end
