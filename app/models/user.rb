d409e44f3b297d8f66f46c754566654c12e80676
hiroko1989 <mwam0615@gmail.com>
2022-07-14 22:30:54 +0900

    ログイン後トップページへ遷移＋バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :ruby_family_name, presence: true
+  validates :ruby_first_name, presence: true
+  validates :birth_date, presence: true
+
+
 end
