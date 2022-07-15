50bcccf42ecb0919e9364be903a367d7c93c9f30
hiroko1989 <mwam0615@gmail.com>
2022-07-15 15:01:31 +0900

    pry.railsインストール

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
  RSpec.describe User, type: :model do
    describe "ユーザー新規登録" do
      it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-       user.valid?
-       binding.pry
-     end
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
+      binding.pry
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+     
      it "emailが空では登録できない" do
      end
-   end
- end
+    end
+  end
