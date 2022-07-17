10e7da661bf700082449977275ca0580ec2196a4
hiroko1989 <mwam0615@gmail.com>
2022-07-17 12:46:46 +0900

    createアクション＋paramsの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,13 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :image, :description, :category_id, :condition_id, :prefecture_id, :shipping_date_id, :delivery_charge_id, :price).merge(user_id: current_user.id)
+  end
+
 end
