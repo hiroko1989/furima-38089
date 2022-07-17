71658cc0b73ef86d090455795fe58248b4925994
hiroko1989 <mwam0615@gmail.com>
2022-07-17 18:46:29 +0900

    モデルの定義/バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,26 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  #テーブル間
+  #テーブル間アソシエーション
   belongs_to :user
   has_one    :purchase
 
-  #Active storage
+  #Active storageのアソシエーション
   has_one_attached :image
 
-  #Active hash
+  #Active hashのアソシエーション
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_charge
   belongs_to :prefecture
   belongs_to :shipping_date
 
+  #「--」を選択した場合にエラーメッセージが出現
   validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :deliverly_charge_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :shipping_date_id, numericality: { other_than: 0 , message: "can't be blank"}
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_17_082341) do
+ActiveRecord::Schema.define(version: 2022_07_17_090346) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2022_07_17_082341) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "conditions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "delivery_charges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -51,6 +61,16 @@ ActiveRecord::Schema.define(version: 2022_07_17_082341) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shipping_dates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
