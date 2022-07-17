abef166f6e6905fc3f67d5307c9ddba6df295c53
hiroko1989 <mwam0615@gmail.com>
2022-07-17 17:19:23 +0900

    Activehashのインストール+アソシエーション追記+name属性の編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,17 @@
 class Item < ApplicationRecord
-  belongs_to: user
-  has_one: purchase
 
+  #テーブル間
+  belongs_to :user
+  has_one    :purchase
+
+  #Active storage
   has_one_attached :image
+
+  #Active hash
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
 end
