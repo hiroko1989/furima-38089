6af0ba3c1eb38291da1f63cc11e4f669d6a3dd87
hiroko1989 <mwam0615@gmail.com>
2022-07-19 15:51:27 +0900

    コードレビューの修正5

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,7 +24,7 @@ class Item < ApplicationRecord
     validates :delivery_charge_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,only_integer: true},format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,only_integer: true}
   end
 
   #「--」を選択した場合に保存できない
