30c3e8fecbd444bd48445143963c98a9df1f5fa9
hiroko1989 <mwam0615@gmail.com>
2022-07-20 18:03:28 +0900

    ルーティングの追記/詳細ページへのリンク設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "items#index"
-  resources :items,only: [:index, :new, :create, :show]
-
+  resources :items do
+    resources :orders, only: [:index, :create]
+   end
 end
