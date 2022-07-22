e6e75b77cbfd8dcfe0de9f9d5ad7560a350c153c
hiroko1989 <mwam0615@gmail.com>
2022-07-22 16:56:06 +0900

    destroyアクションのルーティング設定/パスの指定/destroyアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update,:]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -36,6 +36,9 @@ class ItemsController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
   end
 
   private
