9acb5b11c98dff78129b586ec17c73e9f77333cb
hiroko1989 <mwam0615@gmail.com>
2022-07-22 21:30:24 +0900

    order表記の修正

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,4 +1,4 @@
-class OrderForm
+class PurchaseForm
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
 
@@ -18,8 +18,8 @@ class OrderForm
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
-    Payment.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
   end
-  
+
 end
