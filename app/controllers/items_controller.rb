9d09b55826317a612ef83627d1f3902a020049ae
hiroko1989 <mwam0615@gmail.com>
2022-07-22 16:58:26 +0900

    destroyアクションの再定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update,:]
+  before_action :set_item, only: [:show, :edit, :update,:destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -39,6 +39,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
