5c0422df3a8bdee09ad58eefc581076563d154c6
hiroko1989 <mwam0615@gmail.com>
2022-07-22 23:09:13 +0900

    createアクション追記+ストロングパラメーターの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,6 +6,21 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase_form =PurchaseForm.new(purchase_params)
+    if @purchase_form.valid?
+      pay_item
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
 
 end
