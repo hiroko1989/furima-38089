3febc4098025236a4ab8c6fcf4dd4693f2d871a9
hiroko1989 <mwam0615@gmail.com>
2022-07-26 18:29:47 +0900

    post_numberへname属性修正

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,13 +1,13 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
+  attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :block, :building, :phone_number, :token
 
   with_options presence: true do
   
     validates :user_id
     validates :item_id
  
-    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :block
@@ -19,7 +19,7 @@ class PurchaseForm
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Address.create(purchase_id: purchase.id, postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
+    Address.create(purchase_id: purchase.id, post_number: post_number, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
   end
 
 end
