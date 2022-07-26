6c0c647ec269583c76edd9414b79d8e35c7a8166
hiroko1989 <mwam0615@gmail.com>
2022-07-26 17:30:28 +0900

    商品ページへ遷移するための修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,9 +3,11 @@ class PurchasesController < ApplicationController
 
   def index
     @purchase_form = PurchaseForm.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase_form =PurchaseForm.new(purchase_params)
     if @purchase_form.valid?
       pay_item
@@ -19,8 +21,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_form).permit(:title, :image, :description, :category_id, :condition_id, :prefecture_id, :shipping_date_id, :delivery_charge_id, :price,:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
-
 
 end
